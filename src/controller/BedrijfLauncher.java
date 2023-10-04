65ea47002740192fda49a0862d191a95df1408f7
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2023-10-04 13:58:32 +0200

    9.1.4 Lees afdelingen in uit bestand

--- src/controller/BedrijfLauncher.java
+++ src/controller/BedrijfLauncher.java
@@ -2,6 +2,9 @@ package controller;
 
 import model.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -13,33 +16,21 @@ import java.util.Scanner;
 public class BedrijfLauncher {
 
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
 
-        System.out.print("Geef de naam: ");
-        String naam = keyboard.next();
-        System.out.print("Geef de woonplaats: ");
-        String woonplaats = keyboard.next();
-        System.out.print("Geef de naam van de afdeling: ");
-        String afdelingsNaam = keyboard.next();
-        System.out.print("Geef de plaats van de afdeling: ");
-        String afdelingsPlaats = keyboard.next();
+        ArrayList<Afdeling> afdelingen = new ArrayList<>();
+        File afdelingenBestand = new File("resources/Afdelingen.txt");
 
-        Afdeling afdeling = new Afdeling(afdelingsNaam, afdelingsPlaats);
-        Werknemer werknemer = null;
-
-        while (werknemer == null) {
-            System.out.print("Geef het maandsalaris: ");
-            double maandsalaris = keyboard.nextDouble();
-
-            try {
-                werknemer = new Werknemer(naam, woonplaats, afdeling, maandsalaris);
-                System.out.println(werknemer);
-            } catch (IllegalArgumentException illegalArgumentException) {
-                System.out.println(illegalArgumentException.getMessage());
-            } finally {
-                System.out.println("Je invoer is op de juiste wijze afgehandeld.");
+        try (Scanner afdelingenScanner = new Scanner(afdelingenBestand)) {
+            while (afdelingenScanner.hasNextLine()) {
+                String afdelingsNaam = afdelingenScanner.nextLine();
+                String afdelingsPlaats = afdelingenScanner.nextLine();
+                afdelingen.add(new Afdeling(afdelingsNaam, afdelingsPlaats));
             }
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("Het lukte niet om het bestand te vinden");
         }
+
+        System.out.println(afdelingen);
 
     }
 
