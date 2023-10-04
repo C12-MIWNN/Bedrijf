5aecdea0601cb01d36a70c2807978137fca080b2
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2023-10-04 13:50:14 +0200

    we gaan verder in een andere branch

--- src/controller/BedrijfLauncher.java
+++ src/controller/BedrijfLauncher.java
@@ -4,6 +4,7 @@ import database.AfdelingDAO;
 import database.DBaccess;
 import model.*;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -15,17 +16,6 @@ import java.util.Scanner;
 public class BedrijfLauncher {
 
     public static void main(String[] args) {
-
-        DBaccess dBaccess = new DBaccess("Bedrijf",
-                "userBedrijf",
-                "userBedrijfPW");
-
-        AfdelingDAO afdelingDAO = new AfdelingDAO(dBaccess);
-
-        dBaccess.openConnection();
-//        afdelingDAO.slaAfdelingOp(new Afdeling("HR", "Hilversum"));
-
-        System.out.println(afdelingDAO.geefAfdelingenMetPlaats("Hilversum"));
     }
 
 }
